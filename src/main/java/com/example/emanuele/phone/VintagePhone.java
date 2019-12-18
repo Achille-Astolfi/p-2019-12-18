c95e39f50b905585cf61c8ecbe23971a77a99ede
Roberto.Patane <proberto@example.com>
2019-12-18 17:40:09 +0100

    q

--- src/main/java/com/example/emanuele/phone/VintagePhone.java
+++
@@ -1,6 +0,0 @@
-package com.example.emanuele.phone;
-
-public class VintagePhone extends Phone{
-	public VintagePhone() {}
-	public VintagePhone(String name,String number) { super(name,number); }
-}
